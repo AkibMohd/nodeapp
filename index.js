d4bc802a34fc24ec870131f9bf6d96786cd8dd5b
Mohd Akib <110451218+AkibMohd@users.noreply.github.com>
2023-07-17 14:59:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('welcome to aaqib riyaz channel'); //write a response to the client
+  res.write('welcome to mohd akib channel'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
